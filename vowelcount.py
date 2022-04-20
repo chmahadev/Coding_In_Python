c35462834d75a39d2dfc2222b49c1726ad2b6d4a
chmahadev <89266761+chmahadev@users.noreply.github.com>
2022-04-20 13:17:17 +0530

    Add files via upload

---
+++ vowelcount.py
@@ -0,0 +1,11 @@
+new2 = input("Please enter the word ")
+
+def vowCount(new2):
+    count = 0
+    for j in new2:
+        
+        if(j=='a'or j=='e' or j=='i' or j=='o'or j=='u' or j=='A' or j=='E' or j=='I' or j=='O' or j=='U'):
+            count = count +1
+    return count       
+
+print(vowCount(new2))
