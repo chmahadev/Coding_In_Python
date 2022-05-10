6361b72ee929dd18af0794cdb0b93f70bba669e8
chmahadev <89266761+chmahadev@users.noreply.github.com>
2022-05-10 08:17:20 +0530

    Add files via upload

---
+++ madlibs program.py
@@ -0,0 +1,11 @@
+# madlibs program 
+
+movie = input("movie name: ")
+lead_act = input("lead_role: ")
+fav_actor = input("fav_actor: ")
+year = input("release year: ")
+
+madlib = f"My favorite movie is {movie}! Its lead role was performed by {lead_act}!\
+But my favorite actor from that movie is {fav_actor}! This movie was released in {year}! "
+
+print(madlib)
