db025f95509bc750313b2563241d458734a523e4
chmahadev <89266761+chmahadev@users.noreply.github.com>
2022-06-08 12:25:42 +0530

    Update max profit_1.py

--- max profit_1.py
+++ max profit_1.py
@@ -1,3 +1,12 @@
+#Maximum Profit problem
+#You are given an array A[] of N distinct integers. You can choose two ajecent elemnents, A[i], A[i+1] and replace them with one element 
+#with value equal to the Greatest common divisor of A[i] and A[i+1], profit added in that case will be equal to A[i] + A[i+1]. you can do this  
+#operation until the size of the array becomes 1
+#return the maximum profit we can get
+#Example: Input: N=3, A[1,2,3]
+#output:7
+
+# in this solution i have used list comprehension
 def profit(): #function defination
     #list comprehension for taking input from user
     a = [int(i) for i in input("enter array elements of array using spaces").split()] 
